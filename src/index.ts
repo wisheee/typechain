7935a0947b0ccb8a2f5969488d79dfae4b2ed5fb
wisheee <wishee.u@gmail.com>
2022-03-19 17:01:59 +0900

    feat: create Block 관련 함수 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
@@ -5,6 +7,13 @@ class Block {
   public data: string;
   public timestamp: number;
 
+  static calculateBlockHash = (
+    index: number, 
+    previousHash: string, 
+    timestamp: number, 
+    data: string
+  ) :string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  
   constructor(
     index: number,
     hash: string,
@@ -24,6 +33,32 @@ const genesisBlock: Block = new Block(0, '123456789', '', 'Hello', 20220319);
 
 let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex, 
+    previousBlock.hash, 
+    newTimestamp, 
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex, 
+    newHash, 
+    previousBlock.hash, 
+    data, 
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('hello'), createNewBlock('byebye'));
 
 export {};
