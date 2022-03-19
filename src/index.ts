19b65d497d9246030f67915165c435fadb64af20
wisheee <wishee.u@gmail.com>
2022-03-19 16:01:21 +0900

    feat: tsc-watch 추가 및 script 변경

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}. ${gender}~!`;
+};
+
+console.log(sayHi("wisheee", 5, "female"));
+
+export {};
